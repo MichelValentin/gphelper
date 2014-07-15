e90af55cbc8003489723a79d204322ee3b9b6ee1
Michel Valentin <michel.valentin@gmail.com>
2014-07-15 13:45:37 +0200

    use map instead of array for keys

--- src/gphelper/Gphelper.java
+++ src/gphelper/Gphelper.java
@@ -11,7 +11,6 @@ import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
